2d9467aca73e04dbc012c1cdb800d769e5e1fb92
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-10-24 22:56:43 -0300

    Se arregla un error dejando en funcionamiento

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,4 @@
-const API = 'https://youtube-v3-alternative.p.rapidapi.com/channel?id=UC9k0tTsh_qStx0HPuPftSsg';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCnT575U_9rWDt8SxAfLiIoA&part=snippet%2Cid&order=date&maxResults=12';
 
 const content = null || document.getElementById('content');
 
@@ -6,9 +6,10 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'b41cd3b488msh6f8a8d3e3726d7dp1225bejsndecefd765185',
-		'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 };
+
 
 async function fetchData(urlAPI) {
     const response = await fetch(urlAPI, options);
@@ -21,7 +22,7 @@ async function fetchData(urlAPI) {
     try {
         const videos = await fetchData(API);
         let view = `
-        ${videos.items.map(video =>`
+        ${videos.items.map(video => `
             <div class="group relative">
                 <div
                         class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
@@ -34,9 +35,9 @@ async function fetchData(urlAPI) {
                         </h3>
                 </div>
             </div>
-        `).slice(0.4).join('')}
+        `).slice(0.5).join('')}
         `;
-        content,innerHTML = view;
+        content.innerHTML = view;
     }catch (error) {
         console.log(error);
     }
